f9b708ba722a4ee7aedbb044955316f255957253
ibroxim <ibrohimabdusalomov395@gmail.com>
2023-07-26 21:06:49 +0500

    commit

--- drf/settings.py
+++ drf/settings.py
@@ -33,7 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.sites',
+    # 'django.contrib.sites',
     'django.contrib.staticfiles',
 
     'posts.apps.PostsConfig',
